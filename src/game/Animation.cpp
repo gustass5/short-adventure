88e86f82f746709dd80ac5095aed267e5a2e287e
Gustas <gustass1@gmail.com>
2022-04-04 19:10:15 +0200

    Updated Animation so it can use textures provided from outside

--- src/game/Animation.cpp
+++ src/game/Animation.cpp
@@ -1,15 +1,29 @@
 #include "Animation.hpp"
 
-Animation::Animation(SDL_Renderer* renderer, std::string path, int frameCount, int animationFramesPerSecond) : renderer(renderer),
-																											   animationFramesPerSecond(animationFramesPerSecond) {
-	for (int i = 0; i < frameCount; i++) {
-		this->frames.push_back(TextureManager::LoadTexture(this->renderer, (path + std::to_string(i) + ".png").c_str()));
-	}
+Animation::Animation(SDL_Renderer* renderer,
+					 std::string path,
+					 int frameCount,
+					 int animationFramesPerSecond) : renderer(renderer),
+													 animationFramesPerSecond(animationFramesPerSecond) {
+	this->isStandalone = true;
+	this->frames = Animation::GetAnimationFrames(this->renderer, path, frameCount);
+}
+
+// [WARNING]: If animation is provided with frames from the outside, they have to be freed manually
+Animation::Animation(SDL_Renderer* renderer,
+					 std::vector<SDL_Texture*>& frames,
+					 int animationFramesPerSecond) : renderer(renderer),
+													 frames(frames),
+													 animationFramesPerSecond(animationFramesPerSecond) {
+	this->isStandalone = false;
 }
 
 Animation::~Animation() {
-	for (SDL_Texture* frame : this->frames) {
-		SDL_DestroyTexture(frame);
+	// [WARNING]: Only destroy if animation is standalone
+	if (this->isStandalone) {
+		for (SDL_Texture* frame : this->frames) {
+			SDL_DestroyTexture(frame);
+		}
 	}
 }
 
@@ -25,4 +39,16 @@ void Animation::handleNextFrame() {
 		this->currentFrame = (this->currentFrame + 1) % this->frames.size();
 		this->lastTick = currentTick;
 	}
+}
+
+// [NOTE]: Im using normal pointers and not shared_ptr, because pointers stored are to SDL_Texture and SDL_DestroyTexture() method should be used to remove it.
+// I will avoid freeing this memory by myself, because I'm not sure if SDL_DestroyTexture() does something more behind the scenes.
+std::vector<SDL_Texture*> Animation::GetAnimationFrames(SDL_Renderer* renderer, std::string path, int frameCount) {
+	std::vector<SDL_Texture*> frames;
+
+	for (int i = 0; i < frameCount; i++) {
+		frames.push_back(TextureManager::LoadTexture(renderer, (path + std::to_string(i) + ".png").c_str()));
+	}
+
+	return frames;
 }
--- src/game/Animation.hpp
+++ src/game/Animation.hpp
@@ -13,9 +13,14 @@ class Animation {
 	std::vector<SDL_Texture*> frames;
 	Uint32 lastTick = 0;
 	void handleNextFrame();
+	// [SUMMARY]: Tells if animation owns its own frames. If it does, then it can free them after animation is destroyed.
+	bool isStandalone;
 
   public:
 	Animation(SDL_Renderer* renderer, std::string path, int frameCount, int animationFramesPerSecond);
+	Animation(SDL_Renderer* renderer, std::vector<SDL_Texture*>& frames, int animationFramesPerSecond);
 	~Animation();
 	void render(const SDL_Rect* transform, SDL_RendererFlip flip);
+	// [INFO]: The correct way to copy vector is by returning it by value and not by reference, because compiler deals with it itself.
+	static std::vector<SDL_Texture*> GetAnimationFrames(SDL_Renderer* renderer, std::string path, int frameCount);
 };
