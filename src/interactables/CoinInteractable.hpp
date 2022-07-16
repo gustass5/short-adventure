0d7ff69993daa55ce09fb30806e7d9622306920c
Gustas <gustass1@gmail.com>
2022-07-16 21:23:13 +0300

    Adjusted coin value to 10

--- src/interactables/CoinInteractable.hpp
+++ src/interactables/CoinInteractable.hpp
@@ -8,7 +8,7 @@ class CoinInteractable : public Interactable {
   private:
 	PickableComponent pickable;
 	Player* player;
-	int coinValue = 100;
+	int coinValue = 10;
 
   public:
 	CoinInteractable(SDL_Renderer* renderer, SDL_Texture* sprite, Player* player, int x, int y, int w, int h);
